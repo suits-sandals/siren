8f9741ee44e58e27380c7875ac172cc1bbdc4370
Zachary Brady <zachary@sasbranding.com>
2015-03-17 16:41:51 -0400

    Windows 8 Fix

--- includes/header.php
+++ includes/header.php
@@ -80,6 +80,18 @@
 	    	loadJS( "<?php echo get_bloginfo('template_directory'); ?>/js/global.min.js" );
 	    }
 
+	    //Fix for Windows 8
+	    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+		    var msViewportStyle = document.createElement("style");
+		    msViewportStyle.appendChild(
+		        document.createTextNode(
+		            "@-ms-viewport{width:auto!important}"
+		        )
+		    );
+		    document.getElementsByTagName("head")[0].
+		        appendChild(msViewportStyle);
+		}
+
     </script>
 
 
