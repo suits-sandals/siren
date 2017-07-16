c575928f1f579df1e116ceedebbceac50f2ac919
Miles Blackwood <milesblackwoodmusic@gmail.com>
2017-07-15 23:22:59 -0400

    Formatting.

--- js/jsSrcPreactEx/index.js
+++ js/jsSrcPreactEx/index.js
@@ -1,11 +1,10 @@
 import { h, render } from 'preact';
 
-
-const app = document.getElementsByClassName("app")[0];
+const app = document.getElementsByClassName('app')[0];
 
 function init() {
-	let App = require('./containers/App.jsx').default;
-	root = render(<App />, app.parentNode, app);
+    let App = require('./containers/App.jsx').default;
+    root = render(<App />, app.parentNode, app);
 }
 
 init();
